e59833c09322b4c110ec905a22bd90ce80b6884a
yuxino <nbsawz@gmail.com>
2019-08-15 15:36:03 +0800

    bug version

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,33 @@
 import Taro, { Component, Config } from "@tarojs/taro";
-import { View, Text } from "@tarojs/components";
-import Authority from "../../components/Authority"
+import { View, Text, Block } from "@tarojs/components";
 import "./index.scss";
+
+interface AuthorityProps {
+  renderNoMath: any;
+}
+
+interface AuthorityState {
+  authority: boolean;
+}
+
+class Authority extends Component<AuthorityProps, AuthorityState> {
+  state = {
+    authority: true
+  };
+
+  componentDidMount() {
+    console.log("authority did mount");
+  }
+
+  render() {
+    return (
+      <Block>
+        {this.state.authority ? this.props.children : this.props.renderNoMath}
+      </Block>
+    );
+  }
+}
+
 
 export default class Index extends Component {
   config: Config = {
